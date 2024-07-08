df2dbfce871fc1c67f2861759f5bf4f224856f7d
Shelly Zhu <zhixiuzhu@umail.ucsb.edu>
2024-07-07 18:02:53 -0700

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8GsjUnnI1Y-mhFj-4XCEbM8j7vwIq0lE",
-  authDomain: "bunny-nanny.firebaseapp.com",
-  projectId: "bunny-nanny",
-  storageBucket: "bunny-nanny.appspot.com",
-  messagingSenderId: "640935742680",
-  appId: "1:640935742680:web:b75f4cadbb732e56115bad",
-  measurementId: "G-QGYEGQ5YE5"
+    apiKey: "AIzaSyD8GsjUnnI1Y-mhFj-4XCEbM8j7vwIq0lE",
+    authDomain: "bunny-nanny.firebaseapp.com",
+    projectId: "bunny-nanny",
+    storageBucket: "bunny-nanny.appspot.com",
+    messagingSenderId: "640935742680",
+    appId: "1:640935742680:web:b75f4cadbb732e56115bad",
+    measurementId: "G-QGYEGQ5YE5"
 };
 
 // Initialize Firebase
