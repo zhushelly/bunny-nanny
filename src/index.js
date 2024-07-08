afc9f15781050e54730be08fdef4ee0d2c6b1952
Shelly Zhu <zhixiuzhu@umail.ucsb.edu>
2024-07-08 00:40:48 -0700

    form margin fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './styles.css';
+import './components/styles/styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
