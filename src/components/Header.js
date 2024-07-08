c5ee69925be301e0f9e7b53702b3ba5e16531f99
Michael Hu <michael_hu@ucsb.edu>
2024-07-07 23:26:53 -0700

    styled header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { auth } from '../firebase';
 import { Link, useNavigate } from 'react-router-dom';
+import './Header.css'; // Assuming you save the CSS in a file named Header.css
 
 const Header = ({ user }) => {
   const navigate = useNavigate();
@@ -15,27 +16,29 @@ const Header = ({ user }) => {
   };
 
   return (
-    <header>
-      <div>
+    <header className="header">
+      <div className="header-left">
+        <a href="/register">
+          <img src="link-to-register-icon.png" alt="Register a Bunny" />
+        </a>
+      </div>
+      <div className="header-center">
+        Become a Nanny
+      </div>
+      <div className="header-right">
         {user ? (
           <>
             <span>User ID: {user?.uid}</span>
             <button onClick={handleLogout}>Logout</button>
           </>
         ) : (
+          <span className="login-signup">
           <>
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
           </>
+          </span>
         )}
-      </div>
-      <div>
-        <h1>Need a nanny? Find</h1>
-      </div>
-      <div>
-        <a href="/register">
-          <img src="link-to-register-icon.png" alt="Register a Bunny" />
-        </a>
       </div>
     </header>
   );
